7112cb403e23c97f8b01ada1abb6a9a18bd872b2
mfk <mfk@hengwei.com.cn>
2024-06-23 15:55:24 +0800

    feat: bump boo

--- main.go
+++ main.go
@@ -2,10 +2,6 @@ package gateway
 
 import (
 	"context"
-	"crypto/md5"
-	"crypto/sha1"
-	"crypto/sha256"
-	"crypto/sha512"
 	"encoding/json"
 	"flag"
 	"io/ioutil"
@@ -27,42 +23,34 @@ func Run() {
 	var runner httpext.Runner
 	runner.Flags(flag.CommandLine)
 
-	var filename string
-	flag.StringVar(&filename, "data-file", "", "数据文件")
-
 	var jwtAlg string
 	var jwtSignKey string
 	var jwtVerifyKey string
-	flag.StringVar(&jwtAlg, "jwt-alg", "", "jwt 的算法")
-	flag.StringVar(&jwtSignKey, "jwt-sign-key", "", "jwt 的签名密码")
-	flag.StringVar(&jwtVerifyKey, "jwt-verify-key", "", "jwt 的校验密码")
+	flag.StringVar(&jwtAlg, "auth-jwt-alg", "", "jwt 的算法")
+	flag.StringVar(&jwtSignKey, "auth-jwt-sign-key", "", "jwt 的签名密码")
+	flag.StringVar(&jwtVerifyKey, "auth-jwt-verify-key", "", "jwt 的校验密码")
 
 	var sessionOpt session_auth.Option
 	var sessionHash string
 	var sessionSecretKey string
-	flag.StringVar(&sessionOpt.SessionPath, "session-path", "", "会话的 cookie 路径")
-	flag.StringVar(&sessionOpt.SessionKey, "session-key", "", "会话的 cookie 名称")
-	flag.StringVar(&sessionHash, "session-hash-method", "", "会话的 hash 算法")
-	flag.StringVar(&sessionSecretKey, "session-secret-key", "", "会话的 hash 算法的加密 key")
+	flag.StringVar(&sessionOpt.SessionPath, "auth-session-path", "", "会话的 cookie 路径")
+	flag.StringVar(&sessionOpt.SessionKey, "auth-session-key", "", "会话的 cookie 名称")
+	flag.StringVar(&sessionHash, "auth-session-hash-method", "", "会话的 hash 算法")
+	flag.StringVar(&sessionSecretKey, "auth-session-secret-key", "", "会话的 hash 算法的加密 key")
+
+	var filename string
+	flag.StringVar(&filename, "data-file", "", "数据文件")
 
 	flag.Parse()
 
 	jwtConfig := jwt_auth.NewJWTAuth(jwtAlg, []byte(jwtSignKey), []byte(jwtVerifyKey))
 
-	switch sessionHash {
-	case "":
-	case "md5":
-		sessionOpt.SessionHash = md5.New
-	case "sha1":
-		sessionOpt.SessionHash = sha1.New
-	case "sha256":
-		sessionOpt.SessionHash = sha256.New
-	case "sha512":
-		sessionOpt.SessionHash = sha512.New
-	default:
+	if h, err := session_auth.GetHash(sessionHash); err != nil {
 		runner.Logger.Warn("参数 'session-hash-method' 的值是未知的 hash 算法", slog.Any("method", sessionHash))
 		os.Exit(10002)
 		return
+	} else {
+		sessionOpt.SessionHash = h
 	}
 	sessionOpt.SecretKey = []byte(sessionSecretKey)
 
